a381159269408265f24ac29a405c66eb0d0d5590
dragonlab09-rgb <dragonlab09@gmail.com>
2025-10-20 20:19:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/[EL_NOMBRE_DE_TU_REPOSITORIO]/'
+  base: '/TP-electiva-1/'
 })
